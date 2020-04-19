9ce973816639eadcfafbd141c7e2c9fbaab62c2f
Emil Goldsmith Olesen <emilgoldsmith94@gmail.com>
2020-04-19 19:03:25 +0100

    Add a neat feature showing verification during the test

--- assets/cubeFunctions.js
+++ assets/cubeFunctions.js
@@ -3,22 +3,41 @@
  * @param {string} alg - Algorithm to state to be scrambled too
  */
 function getScramble(alg) {
-  return new Promise(resolve => {
-    if (alg) Cube.asyncSolve(new Cube().move(alg), resolve);
+  return new Promise((resolve) => {
+    if (alg) new Cube().move(alg).asyncSolve(resolve);
     else Cube.asyncScramble(resolve);
+  });
+}
+
+function getSolution(scramble) {
+  return new Promise((resolve) => {
+    new Cube().move(scramble).asyncSolve(resolve);
   });
 }
 
 function displayScramble(alg) {
   document.getElementById("scrambleContainer").style.display = "block";
   document.getElementById("scrambleText").innerText = alg;
-  const urlFriendlyAlg = alg.replace(/\s+/g, ""); // remove spaces
-  const baseUrl =
-    "http://cube.crider.co.uk/visualcube.png?bg=t&sch=wrgyob&size=150&alg=" +
-    urlFriendlyAlg;
+  const baseUrl = getDisplayCubeUrl(alg);
   const imageTags = document.querySelectorAll("#scrambleContainer img");
   imageTags[0].src = baseUrl;
   imageTags[1].src = `${baseUrl}x2y'`;
 }
 
+function displayVerificationCube(alg) {
+  document.querySelector(".verificationcube").style.display = "block";
+  const baseUrl = getDisplayCubeUrl(alg);
+  const imageTags = document.querySelectorAll(".verificationcube img");
+  imageTags[0].src = baseUrl;
+  imageTags[1].src = `${baseUrl}x2y'`;
+}
+
 executeCubeFn(() => getScramble().then(displayScramble));
+
+function getDisplayCubeUrl(alg) {
+  const urlFriendlyAlg = alg.replace(/\s+/g, ""); // remove spaces
+  const baseUrl =
+    "http://cube.crider.co.uk/visualcube.png?bg=t&sch=wrgyob&size=150&alg=" +
+    urlFriendlyAlg;
+  return baseUrl;
+}
--- assets/cubejs/async.js
+++ assets/cubejs/async.js
@@ -1,21 +1,21 @@
-(function() {
+(function () {
   var Cube, Extend, Include, key, value;
 
   Cube = this.Cube || require("./cube");
 
   Extend = {
     asyncOK: !!window.Worker,
-    _asyncSetup: function(workerURI) {
+    _asyncSetup: function (workerURI) {
       if (this._worker) {
         return;
       }
       this._worker = new window.Worker(workerURI);
-      this._worker.addEventListener("message", e => {
+      this._worker.addEventListener("message", (e) => {
         return this._asyncEvent(e);
       });
       return (this._asyncCallbacks = {});
     },
-    _asyncEvent: function(e) {
+    _asyncEvent: function (e) {
       var callback, callbacks;
       callbacks = this._asyncCallbacks[e.data.cmd];
       if (!(callbacks && callbacks.length)) {
@@ -25,46 +25,46 @@
       callbacks.splice(0, 1);
       return callback(e.data);
     },
-    _asyncCallback: function(cmd, callback) {
+    _asyncCallback: function (cmd, callback) {
       var base;
       (base = this._asyncCallbacks)[cmd] || (base[cmd] = []);
       return this._asyncCallbacks[cmd].push(callback);
     },
-    asyncInit: function(workerURI, callback) {
+    asyncInit: function (workerURI, callback) {
       this._asyncSetup(workerURI);
-      this._asyncCallback("init", function() {
+      this._asyncCallback("init", function () {
         return callback();
       });
       return this._worker.postMessage({
-        cmd: "init"
+        cmd: "init",
       });
     },
-    _asyncSolve: function(cube, callback) {
+    _asyncSolve: function (cube, callback) {
       this._asyncSetup();
-      this._asyncCallback("solve", function(data) {
+      this._asyncCallback("solve", function (data) {
         return callback(data.algorithm);
       });
       return this._worker.postMessage({
         cmd: "solve",
-        cube: cube.toJSON()
+        cube: cube.toJSON(),
       });
     },
-    asyncScramble: function(callback) {
+    asyncScramble: function (callback) {
       this._asyncSetup();
-      this._asyncCallback("solve", function(data) {
+      this._asyncCallback("solve", function (data) {
         return callback(Cube.inverse(data.algorithm));
       });
       return this._worker.postMessage({
         cmd: "solve",
-        cube: Cube.random().toJSON()
+        cube: Cube.random().toJSON(),
       });
-    }
+    },
   };
 
   Include = {
-    asyncSolve: function(callback) {
+    asyncSolve: function (callback) {
       return Cube._asyncSolve(this, callback);
-    }
+    },
   };
 
   for (key in Extend) {
